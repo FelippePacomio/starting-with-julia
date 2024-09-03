8b5434657f2fb3e139e4c6649500b98ec0271844
Felippe Pacomio <felipepacomio@gmail.com>
2024-09-02 22:03:49 -0300

    Static or Dinamic Testing

---
+++ testagem-de-tipo2.jl
@@ -0,0 +1,13 @@
+#Testagem de tipos (Estático X Dinâmico)
+
+a = 123 
+b = 'a'
+c = false
+
+println(a, " Type: ", typeof(a))
+println(b, " Type: ", typeof(b))
+println(c, " Type: ", typeof(c))
+
+println("\n\n")
+println("Não é necessário a declaração do tipo: Tipagem dinâmica")
+
